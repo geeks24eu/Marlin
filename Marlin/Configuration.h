3320f5c7e8c437d3b11f47f311176aa50135adc1
GEEKS24eu <annamrafal@geeks24.eu>
2021-11-13 11:11:55 +0100

    Changed Z max position

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1391,7 +1391,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS (X_BED_SIZE+X_MIN_POS)
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 290
+#define Z_MAX_POS 280
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
